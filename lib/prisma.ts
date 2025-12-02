ccac9bf22b2e1772511e1d7b5f6cebb5abf3b54c
Your Name <devonulyate@gmail.com>
2025-12-02 11:47:07 +0200

    Add prisma adapter pg

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -8,6 +10,7 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['warn', 'error'],
+    adapter: new PrismaPg(new Pool({ connectionString: process.env.DATABASE_URL }))
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
